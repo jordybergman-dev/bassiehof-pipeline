a1ace423886f24c7bae4d0affa03142f2f751d1c
root <root@vmi3147169.contaboserver.net>
2026-03-13 20:50:20 +0100

    Fix week summary for Friday (back) and Monday (forward)

---
+++ x_poster.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+"""
+X/Twitter Poster - Automatisch posten naar @bassiehof
+"""
+import os
+import requests
+from requests_oauthlib import OAuth1
+
+# Credentials from environment
+CONSUMER_KEY = os.environ.get("X_CONSUMER_KEY", "4DHzgY5szwxKGg1XMG6L84emn")
+CONSUMER_SECRET = os.environ.get("X_CONSUMER_SECRET", "b8czGuw3CVXXo6q7JLNjFyBrGGZg0abDGnIItbNzY6AhdiyCG2")
+ACCESS_TOKEN = os.environ.get("X_ACCESS_TOKEN", "1701510127746965504-60tQ6PdnNfAnmvq8kjyGfSKqwRYnlm")
+ACCESS_SECRET = os.environ.get("X_ACCESS_SECRET", "zfDe9DwaRM0L89kP90WyurSo26XhgOqMUaMeQuxelFieK")
+
+auth = OAuth1(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET)
+
+def post_text(text):
+    """Post tekst naar X"""
+    url = "https://api.twitter.com/2/tweets"
+    data = {"text": text}
+    r = requests.post(url, auth=auth, json=data)
+    return r.json()
+
+def post_with_media(text, media_path=None):
+    """Post met media"""
+    if media_path:
+        # Upload media first
+        url = "https://upload.twitter.com/1.1/media/upload.json"
+        with open(media_path, 'rb') as f:
+            files = {'media': f}
+            media = requests.post(url, auth=auth, files=files).json()
+        
+        media_id = media['media_id_string']
+        
+        # Post tweet with media
+        url = "https://api.twitter.com/2/tweets"
+        data = {"text": text, "media": {"media_ids": [media_id]}}
+    else:
+        url = "https://api.twitter.com/2/tweets"
+        data = {"text": text}
+    
+    r = requests.post(url, auth=auth, json=data)
+    return r.json()
+
+def post_video_update(video_title, youtube_url):
+    """Post video update voor Bassiehof"""
+    hook = "🔥 Nieuwe video!"
+    text = f"{hook}\n\n{video_title}\n\n👇 Kijk hier:\n{youtube_url}\n\n#Bassiehof #Politiek"
+    
+    return post_text(text)
+
+# Test
+if __name__ == "__main__":
+    print("X Poster klaar!")
+    # Test post
+    # result = post_text("Test bericht van Bassiehof Pipeline! 🤖")
+    # print(result)
